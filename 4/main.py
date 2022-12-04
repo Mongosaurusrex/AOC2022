63d69c4cb585c335edd7f55da7a30fe1bc6773c4
Nathan Dygant <nathan.dygant@gmail.com>
2022-12-04 17:36:18 +0100

    Day 4 🎁w

---
+++ 4/main.py
@@ -0,0 +1,46 @@
+def day_four():
+    section_pairs = []
+    print("Day four")
+    def setup():
+        nonlocal section_pairs
+        with open("./4/input.txt") as f:
+            lines = f.readlines()
+            for line in lines:
+                line = line.strip()
+                first, second = line.split(",")
+                section_pairs.append((first, second))
+
+    
+    def part_one():
+        total_overlaps = 0
+        for l, r in section_pairs:
+            l1, l2 = l.split("-")
+            r1, r2 = r.split("-")
+
+            if int(l1) <= int(r1) and int(l2) >= int(r2):
+                total_overlaps += 1
+            elif int(r1) <= int(l1) and int(r2) >= int(l2):
+                total_overlaps += 1
+
+        print(f"\tFirst solution: {total_overlaps}")
+
+    def part_two():
+        total_overlaps = 0
+        for l, r in section_pairs:
+            l1, l2 = l.split("-")
+            r1, r2 = r.split("-")
+            
+            l_set = set([*range(int(l1), int(l2) + 1)])
+            r_set = set([*range(int(r1), int(r2) + 1)])
+
+            total_overlaps += 1 if (l_set & r_set) else 0
+
+        print(f"\tSecond solution: {total_overlaps}")
+
+    setup()
+    part_one()
+    part_two()
+
+if __name__ == "__main__":
+    day_four()
+
--- Makefile
+++ Makefile
@@ -1,6 +1,6 @@
 MAKEFLAGS += --silent
 
-run: day_one day_two day_three
+run: day_one day_two day_three day_four
 	cat tree.txt
 day_one:
 	python3 ./1/main.py
@@ -8,3 +8,5 @@ day_two:
 	python3 ./2/main.py
 day_three:
 	python3 ./3/main.py
+day_four:
+	python3 ./4/main.py
